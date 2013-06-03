e053c0cb62c0706e4e9cf52d98a7604b0c0fb786
SAKAMOTO Kazunori <exkazuu@gmail.com>
2013-06-03 00:36:37 -0700

    Restore tests for HUnit.

--- Test/SortTest.hs
+++ Test/SortTest.hs
@@ -1,17 +1,10 @@
 module SortTest where
 
-import Test.Framework
-import Test.Framework.Providers.HUnit
-import Test.HUnit
 import Util.Sort
+import Test.HUnit
 
-tests2 = test [ "test1" ~: "quicksort [0]," ~: [0] ~=? (quicksort [0]),
+tests = test [ "test1" ~: "quicksort [0]," ~: [0] ~=? (quicksort [0]),
                "test2" ~: "quicksort [1,0,2]," ~: [0,1,2] ~=? (quicksort [1,0,2]) ]
-
-import qualified Distribution.TestSuite as Cabal
-import qualified Distribution.TestSuite.HUnit as CabalHUnit
-
-tests = map (\(x,y) -> CabalHUnit.test x y) [("Login tests", test2)]
 
 -- test1 = TestCase (assertEqual "quicksort [0]," [0] (quicksort [0]))
 -- test2 = TestCase (assertEqual "quicksort [1,0,2]," [0,1,2] (quicksort [1,0,2]))
