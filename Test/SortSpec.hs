96fed7cb166da8f49b3ffcc01f02ed18a38d0e21
SAKAMOTO Kazunori <exkazuu@gmail.com>
2013-06-02 23:59:12 -0700

    Add tests using QuickCheck.

--- Test/SortSpec.hs
+++ Test/SortSpec.hs
@@ -1,7 +1,10 @@
 module SortSpec where
 
+import Data.List
 import Util.Sort
 import Test.Hspec
+import Test.Hspec.QuickCheck (prop)
+import Test.QuickCheck hiding ((.&.))
 
 spec :: Spec
 spec = do
@@ -10,3 +13,7 @@ spec = do
       quicksort [0] `shouldBe` [0]
     it "sorts [1,0,2]" $
       quicksort [1,0,2] `shouldBe` [0,1,2]
+    prop "works as well as " $ \xs ->
+      quicksort xs == sort (xs :: [Int])
+    prop "sorts lists many times" $ \xs ->
+      quicksort (quicksort xs) == quicksort (xs :: [Int])
